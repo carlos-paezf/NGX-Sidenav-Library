f8bb864ad6f9742f566997da7d22d2ea4a7c2f30
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-15 14:01:11 -0500

    Update Library Module

--- projects/ngx-sidenav/src/lib/ngx-sidenav.module.ts
+++ projects/ngx-sidenav/src/lib/ngx-sidenav.module.ts
@@ -1,18 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+
+import { NgxSidenavMenuItemComponent } from './components/ngx-sidenav-menu-item/ngx-sidenav-menu-item.component';
+import { NgxSidenavPanelComponent } from './components/ngx-sidenav-panel/ngx-sidenav-panel.component';
 
 
 @NgModule( {
     declarations: [
+        NgxSidenavPanelComponent,
+        NgxSidenavMenuItemComponent
     ],
     imports: [
+        CommonModule,
+        RouterModule,
         MatSidenavModule,
         MatButtonModule,
         MatIconModule
     ],
     exports: [
+        NgxSidenavPanelComponent,
     ]
 } )
 export class NgxSidenavModule { }
