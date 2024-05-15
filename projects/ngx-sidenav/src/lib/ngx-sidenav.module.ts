1497f3c3ef83a0bfee02f5d3f1e52f7863b7b50d
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-15 13:47:14 -0500

    Add Angular Material

--- projects/ngx-sidenav/src/lib/ngx-sidenav.module.ts
+++ projects/ngx-sidenav/src/lib/ngx-sidenav.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule( {
     declarations: [
     ],
     imports: [
+        MatSidenavModule,
+        MatButtonModule,
+        MatIconModule
     ],
     exports: [
     ]
