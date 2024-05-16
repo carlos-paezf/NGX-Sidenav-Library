fb00c187feb5b5699c7af122af4d05a1988fe0d9
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-15 19:02:32 -0500

    Add Library Implementation

--- projects/showcase/src/app/app.module.ts
+++ projects/showcase/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgxSidenavModule } from 'ngx-sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialDesignModule } from './material-design.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+
+@NgModule( {
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        NgxSidenavModule,
+        MaterialDesignModule
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
