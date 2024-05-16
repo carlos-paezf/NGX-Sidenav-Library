04445090961d2217cb81f4bc8e4903852cdbcdbe
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-15 19:28:09 -0500

    Export Component

--- projects/ngx-sidenav/src/public-api.ts
+++ projects/ngx-sidenav/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-sidenav
  */
 
-export * from './lib/ngx-sidenav.module';
 export * from './lib/types/index';
+export * from './lib/ngx-sidenav.module';
+export * from './lib/components/ngx-sidenav-panel/ngx-sidenav-panel.component';
